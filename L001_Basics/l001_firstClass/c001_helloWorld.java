86d4a28921473ea6d5a5794b598caacaac557e55
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-08 22:06:21 +0530

    first Class

---
+++ L001_Basics/l001_firstClass/c001_helloWorld.java
@@ -0,0 +1,9 @@
+public class c001_helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Geekster 1");
+        System.out.println("Hello Geekster 2");
+        System.out.print("Hello Geekster 3");
+        System.out.println("Hello Geekster 4");
+        System.out.print("Hello Geekster 5");
+    }
+}
---
+++ L001_Basics/l001_firstClass/c002_SumOfTwoNumber.java
@@ -0,0 +1,17 @@
+public class c002_SumOfTwoNumber {
+    public static void main(String[] args) {
+        // for comment: ctrl + /
+        // for un-comment: ctrl + /
+
+        // System.out.println("Sum of 2 + 3 is: " + (2 + 3)); // when number intract with string it become string
+        // System.out.println("Sum of 6 + 10 is: " + (6 + 10));
+        // System.out.println("Sum of 16 + 23 is: " + (16 + 23));
+
+        int a = 400;
+        int b = 200;
+        System.out.println("sum of two numbers are: " + (a + b)); // for addition
+        System.out.println("sum of two numbers are: " + (a - b)); // for subtraction
+        System.out.println("sum of two numbers are: " + (a * b)); // for multiplication
+        System.out.println("sum of two numbers are: " + (a / b)); // for divide
+    }
+}
