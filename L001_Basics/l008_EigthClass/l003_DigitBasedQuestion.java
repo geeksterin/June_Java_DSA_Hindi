3436a446c660f6f3bf5be88ebd25843f9cfd6845
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-16 21:52:06 +0530

    update

--- L001_Basics/l008_EigthClass/l003_DigitBasedQuestion.java
+++ L001_Basics/l008_EigthClass/l003_DigitBasedQuestion.java
@@ -5,19 +5,33 @@ public class l003_DigitBasedQuestion {
     public static Scanner scn = new Scanner(System.in);
 
     public static int noOfDigits(int num) {
+        int len = 0;
+        while (num != 0) {
+            num /= 10;
+            len++;
+        }
 
+        return len;
     }
 
     // if you able to locate digit return its index otherwise return -1
-    public static int findDigit(int num,int digit) {
+    public static int findDigit(int num, int digit) {
+        int idx = noOfDigits(num) - 1;
 
+        while (num != 0) {
+            int d = num % 10;
+            if (d == digit)
+                return idx;
+
+            num /= 10;
+            idx--;
+        }
+
+        return -1;
     }
-
-
 
     public static void main(String[] args) {
         int num = scn.nextInt();
-
         System.out.println(noOfDigits(num));
     }
 
