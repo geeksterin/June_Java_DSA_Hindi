caf989c015e7d57174c7a2554ad6ff888648f972
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-16 21:10:08 +0530

    update

--- L001_Basics/l008_EigthClass/l002_fibonacci.java
+++ L001_Basics/l008_EigthClass/l002_fibonacci.java
@@ -4,6 +4,16 @@ public class l002_fibonacci {
 
     public static Scanner scn = new Scanner(System.in);
 
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+
     public static void main(String[] args) {
         int n = scn.nextInt();
         printFibonacci(n);
---
+++ L001_Basics/l008_EigthClass/l003_DigitBasedQuestion.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class l003_DigitBasedQuestion {
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int noOfDigits(int num) {
+
+    }
+
+    // if you able to locate digit return its index otherwise return -1
+    public static int findDigit(int num,int digit) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int num = scn.nextInt();
+
+        System.out.println(noOfDigits(num));
+    }
+
+}
