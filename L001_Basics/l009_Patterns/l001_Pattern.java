300bf98af07844722f425b8da83b97a65e1fdfee
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-17 20:37:42 +0530

    update

--- L001_Basics/l009_Patterns/l001_Pattern.java
+++ L001_Basics/l009_Patterns/l001_Pattern.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class l001_Pattern {
     public static Scanner scn = new Scanner(System.in);
 
-    public static void pattern(int n) {
+    public static void pattern_01(int n) {
         int nsp = n - 1, nst = 1; // nst : no of star, nsp : no of space
 
         for (int r = 1; r <= n; r++) {
             for (int csp = 1; csp <= nsp; csp++) { // current space count
-                System.out.print(" ");
+                System.out.print("  ");
             }
 
             for (int cst = 1; cst <= nst; cst++) {  // current star count
@@ -17,12 +17,14 @@ public class l001_Pattern {
 
             nsp--;
             nst++;
+            System.out.println();
         }
 
     }
 
     public static void main(String[] args) {
         int n = scn.nextInt();
+        pattern_01(n);
 
     }
 }
