30b13d2e07dd799d57d4b2c8998a56f6b79065c1
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-17 21:46:39 +0530

    update

--- L001_Basics/l009_Patterns/l001_Pattern.java
+++ L001_Basics/l009_Patterns/l001_Pattern.java
@@ -35,12 +35,76 @@ public class l001_Pattern {
             nsp++;
             System.out.println();
         }
+    }
 
+    public static void pattern_03(int n) {
+        int nst = 1, nsp = 0;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++)
+                System.out.print("  ");
+
+            for (int cst = 1; cst <= nst; cst++)
+                System.out.print("* ");
+
+            nst++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern_04(int n) {
+        int nst = n;
+        for (int r = 1; r <= n; r++) {
+            for (int cst = 1; cst <= nst; cst++)
+                System.out.print("* ");
+
+            nst--;
+            System.out.println();
+        }
+    }
+
+    public static void pattern_05(int n) {
+        int nst1 = 1, nsp1 = n - 1;
+        int nst2 = 1, nsp2 = n - 1;
+        for (int r = 1; r <= n; r++) {
+            for (int cst = 1; cst <= nst1; cst++)
+                System.out.print("* ");
+
+            for (int csp = 1; csp <= nsp1; csp++)
+                System.out.print("  ");
+
+            for (int csp = 1; csp <= nsp2; csp++)
+                System.out.print("  ");
+
+            for (int cst = 1; cst <= nst2; cst++)
+                System.out.print("* ");
+
+            nst1++;
+            nsp1--;
+            nst2++;
+            nsp2--;
+            System.out.println();
+        }
+    }
+
+    public static void pattern_05_01(int n) {
+        int nst = 1, nsp = 2 * n - 2;
+        for (int r = 1; r <= n; r++) {
+            for (int cst = 1; cst <= nst1; cst++)
+                System.out.print("* ");
+
+            for (int csp = 1; csp <= nsp2; csp++)
+                System.out.print("  ");
+
+            for (int cst = 1; cst <= nst2; cst++)
+                System.out.print("* ");
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         int n = scn.nextInt();
-        pattern_02(n);
+        pattern_05(n);
 
     }
 }
