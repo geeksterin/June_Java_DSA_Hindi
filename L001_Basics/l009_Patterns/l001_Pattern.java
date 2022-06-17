5774cc917606051cedef5798e88cfb44a2678fce
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-17 20:29:34 +0530

    update

---
+++ L001_Basics/l009_Patterns/l001_Pattern.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class l001_Pattern {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void pattern(int n) {
+        int nsp = n - 1, nst = 1; // nst : no of star, nsp : no of space
+
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) { // current space count
+                System.out.print(" ");
+            }
+
+            for (int cst = 1; cst <= nst; cst++) {  // current star count
+                System.out.print("* ");
+            }
+
+            nsp--;
+            nst++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n = scn.nextInt();
+
+    }
+}
