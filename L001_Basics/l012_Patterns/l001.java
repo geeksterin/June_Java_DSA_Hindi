80c2015045c155b10181120f05da068f10138a4b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-21 21:34:18 +0530

    update

--- L001_Basics/l012_Patterns/l001.java
+++ L001_Basics/l012_Patterns/l001.java
@@ -24,9 +24,65 @@ public class l001 {
         }
     }
 
+    public static void diamond(int n) {
+        if (n % 2 == 0) // if n is even make it odd
+            n++;
+
+        int nst = 1, nsp = n / 2;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++)
+                System.out.print("  ");
+
+            for (int cst = 1; cst <= nst; cst++)
+                System.out.print("* ");
+
+            if (r <= n / 2) {
+                nst += 2;
+                nsp--;
+            } else {
+                nst -= 2;
+                nsp++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamondNumber(int n) {
+        if (n % 2 == 0) // if n is even make it odd
+            n++;
+
+        int nst = 1, nsp = n / 2;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++)
+                System.out.print("  ");
+
+            int count = r;
+            if (r > n / 2 + 1)
+                count = n - r + 1;
+
+            for (int cst = 1; cst <= nst; cst++){
+                System.out.print(count + " ");
+                if(??????){
+                    count++;
+                }else{
+                    count--;
+                }
+            }
+
+            if (r <= n / 2) {
+                nst += 2;
+                nsp--;
+            } else {
+                nst -= 2;
+                nsp++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int n = scn.nextInt();
-        hourGlass(n);
+        diamondNumber(n);
 
     }
 }
