0d056caeaee37292c4cc55975a19cc31614d20b3
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-22 20:37:50 +0530

    update

--- L001_Basics/l013_ForLoopQuestions/l001.java
+++ L001_Basics/l013_ForLoopQuestions/l001.java
@@ -1,20 +1,39 @@
 import java.util.*;
 
 public class l001 {
-    public static int power(int a,int b){
+    public static int power(int a, int b) {
+        int ans = 1;
+        for (int i = 1; i <= b; i++) {
+            ans *= a;
+        }
+
+        return ans;
+    }
+
+    public static int fact(int n) {
+        int ans = 1;
+        while (n > 0) {
+            ans *= n--;
+        }
+
+        return ans;
 
     }
 
-    public static int fact(int n){
+    public static int ncr(int n, int r) {
+        int a = fact(n);
+        int b = fact(n - r);
+        int c = fact(r);
+
 
     }
 
-    public static int ncr(int n,int r){
+    public static int npr(int n,int r){
+        
 
     }
 
     public static void main(String[] args) {
-        
 
     }
 }
