2ed29d3884fa80f8282a68ad8246bdb67693ded9
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-22 20:16:12 +0530

    update

---
+++ L001_Basics/l013_ForLoopQuestions/l001.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class l001 {
+    public static int power(int a,int b){
+
+    }
+
+    public static int fact(int n){
+
+    }
+
+    public static int ncr(int n,int r){
+
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
+}
