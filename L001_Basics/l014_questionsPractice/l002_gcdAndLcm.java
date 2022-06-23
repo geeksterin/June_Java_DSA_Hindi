a4388f76787db6928a5485e88cb0bcf9c0a5b7d3
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-23 21:32:05 +0530

    update

--- L001_Basics/l014_questionsPractice/l002_gcdAndLcm.java
+++ L001_Basics/l014_questionsPractice/l002_gcdAndLcm.java
@@ -1,13 +1,34 @@
-public class l002_gcdAndLcm{
-    
-    public static int gcd(int a,int b){
+import java.util.*;
 
+public class l002_gcdAndLcm {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int gcd(int a, int b) {
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b % a != 0) {
+            int rem = b % a;
+            b = a;
+            a = rem;
+        }
+
+        return a;
     }
 
-    public static void main(String[] args){
-        
-
+    public static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        return (a / gcd) * b;
     }
 
-    
+    public static void main(String[] args) {
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+
+        System.out.println(lcm(a, b));
+    }
+
 }
