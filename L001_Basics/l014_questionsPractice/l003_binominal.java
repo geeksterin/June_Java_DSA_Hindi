205e43ca98cfb9578b56005fc21caa9f5c1ed0c7
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-23 21:53:21 +0530

    update

---
+++ L001_Basics/l014_questionsPractice/l003_binominal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class l003_binominal {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int binomial(int b, int n) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int a = scn.nextInt();
+        int n = scn.nextInt();
+        System.out.println(a, n);
+    }
+
+}
