a549a2e782e31ee0a48696964bb6d0b416f86062
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-28 21:57:58 +0530

    update

--- L001_Basics/l017_String/l001_basicQuestion.java
+++ L001_Basics/l017_String/l001_basicQuestion.java
@@ -16,21 +16,42 @@ public class l001_basicQuestion {
 
     public static void printSmallUpperCase(String str) {
         int smallCaseChar = 0, upperCaseChar = 0;
-        // code
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z')
+                smallCaseChar++;
+            else if (ch >= 'A' && ch <= 'Z')
+                upperCaseChar++;
+        }
+
         System.out.println("Small Character Count: " + smallCaseChar);
         System.out.println("Upper Character Count: " + upperCaseChar);
     }
 
     // abADv -> ABadV
     public static String caseConversion(String str) {
-        
+        String ans = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                ans += (char) (ch - 'a' + 'A');
+            } else if (ch >= 'A' && ch <= 'Z') {
+                ans += (char) (ch - 'A' + 'a');
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
-        String str = scn.nextLine();
-        char ch = scn.next().charAt(0);
+        // String str = scn.nextLine();
+        // char ch = scn.next().charAt(0);
 
-        System.out.println(countChar(str, ch));
+        // System.out.println(countChar(str, ch));
+
+        // printSmallUpperCase(scn.next());
+        String ans = caseConversion("aBVJHAGSaaajhsASAAUYTYJUsdfcsf");
+        System.out.println(ans);
     }
 
 }
