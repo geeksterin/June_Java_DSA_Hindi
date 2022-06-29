8f9b6a6a7fb297529a883a65d8ab9c6907f1b102
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-06-29 21:58:08 +0530

    update

--- L001_Basics/l018_Array/l001_basicArray.java
+++ L001_Basics/l018_Array/l001_basicArray.java
@@ -18,13 +18,39 @@ public class l001_basicArray {
 
     // return index if data found, if not then return -1;
     public static int findElement(int[] arr, int data) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == data) {
+                return i;
+            }
+        }
 
+        return -1;
+    }
+
+    public static int maxEle(int[] arr) {
+        int maxEle = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            // maxEle = Math.max(maxEle, arr[i]);
+            maxEle = arr[i] > maxEle ? arr[i] : maxEle;
+        }
+        return maxEle;
+    }
+
+    public static int minEle(int[] arr) {
+        int minEle = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            // minEle = Math.min(minEle, arr[i]);
+            minEle = arr[i] < minEle ? arr[i] : minEle;
+        }
+        return minEle;
     }
 
     public static void main(String[] args) {
         int n = scn.nextInt();
         int[] arr = new int[n]; // int[] arr = {10,20,-40,50,70,90,-89,-56};
         input1D(arr);
-        print1D(arr);
+        // print1D(arr);
+        int ans = findElement(arr, scn.nextInt());
+        System.out.println("index: " + ans);
     }
 }
