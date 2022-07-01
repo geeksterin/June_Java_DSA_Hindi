a4396638173509b783c2c1596120f47ea87e451d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-01 20:17:15 +0530

    update

---
+++ L001_Basics/l020_Arrays/l001_questions.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class l001_questions {
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void input1D(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt(); // write
+        }
+    }
+
+    public static void print1D(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " "); // read
+        }
+        System.out.println();
+    }
+
+    // A and B are the arrays of digit.
+    public static int[] sumOftwoArrays(int[] A, int[] B) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
