a169656b6a9fcc884748c21ef0986152814715fb
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-06 21:16:26 +0530

    update

--- L001_Basics/l023_ArraysSorting/l001_questions.java
+++ L001_Basics/l023_ArraysSorting/l001_questions.java
@@ -3,21 +3,52 @@ import java.util.*;
 public class l001_questions {
     public static Scanner scn = new Scanner(System.in);
 
-    public static int maximum(int[] arr) {
-
+    public static int getMaximum(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int ele : arr) {
+            // max = ele > max ? ele : max;
+            max = Math.max(ele, max);
+        }
+        return max;
     }
 
-    public static int minimum(int[] arr) {
-
+    public static int getMinimum(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int ele : arr) {
+            // min = ele < min ? ele : min;
+            min = Math.min(ele, min);
+        }
+        return min;
     }
 
-    // O(2*N)
+    // O(2*N): two traveral method
     public static int findDiffrence_01(int[] arr) {
+        int max = getMaximum(arr);
+        int min = getMinimum(arr);
 
+        return max - min;
     }
 
-    // O(1*N)
+    // O(1*N): one traveral method
     public static int findDiffrence_02(int[] arr) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int ele : arr) {
+            max = Math.max(max, ele);
+            min = Math.min(min, ele);
+        }
+
+        // for(int i = 0; i < arr.length;i++){
+        //     max = Math.max(max, arr[i]);
+        //     min = Math.min(min, arr[i]);
+        // }
+
+        return max - min;
+    }
+
+    public static int countPeakEle(int[] arr){
+        
 
     }
+
+
 }
