1dac8d29344ec197a9a08bc56c856a7fd2d10318
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-06 20:18:04 +0530

    update

---
+++ L001_Basics/l023_ArraysSorting/l001_questions.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class l001_questions {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int maximum(int[] arr) {
+
+    }
+
+    public static int minimum(int[] arr) {
+
+    }
+
+    // O(2*N)
+    public static int findDiffrence_01(int[] arr) {
+
+    }
+
+    // O(1*N)
+    public static int findDiffrence_02(int[] arr) {
+
+    }
+}
