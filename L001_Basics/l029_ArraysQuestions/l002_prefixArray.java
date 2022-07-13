293d914114cd2c5821b005de79e5ed6dcbb878f5
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-13 21:56:02 +0530

    update

--- L001_Basics/l029_ArraysQuestions/l002_prefixArray.java
+++ L001_Basics/l029_ArraysQuestions/l002_prefixArray.java
@@ -3,11 +3,26 @@ import java.util.*;
 public class l002_prefixArray {
     private static Scanner scn = new Scanner(System.in);
 
-    public static int prefixArray(int[] arr) {
+    public static int sumInRange(int[] arr, int li, int ri) {
+        int sum = 0;
+        while (li <= ri) {
+            sum += arr[li++];
+        }
+
+        return sum;
+    }
+
+    public static void prefixArray(int[] arr) {
+        int n = arr.length;
+        int[] parr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            parr[i + 1] = parr[i] + arr[i];
+        }
+
         int q = scn.nextInt();
         while (q-- > 0) {
             int lr = scn.nextInt(), rr = scn.nextInt();
-            int sum = ??;
+            int sum = parr[rr + 1] - parr[lr];
             System.out.println(sum);
         }
     }
@@ -21,8 +36,9 @@ public class l002_prefixArray {
     }
 
     public static void main(String[] args) {
+
+        
         int[] arr = { 4, 6, 8, 9, 7, 10, 12, 13, 16, 17, 22 };
-        int tar = 30;
-        threeSum(arr, tar);
+        prefixArray(arr);
     }
 }
