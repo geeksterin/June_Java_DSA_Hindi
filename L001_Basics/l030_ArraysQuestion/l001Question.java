360d65faa8020e8ea59860d6e0139af3570b3485
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-14 20:55:57 +0530

    update

---
+++ L001_Basics/l030_ArraysQuestion/l001Question.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class l001Question {
+    public static int[] mergeTwoSortedArrays(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        if (n == 0 || m == 0)
+            return n != 0 ? A : B;
+
+        int len = n + m;
+        int[] ans = new int[len];
+        int i = 0, j = 0, k = 0;
+        while(i < n || j < m){
+
+        }
+
+        while(i < n){
+
+        }
+
+        while(j < m){
+
+        }
+
+        return ans;
+
+    }
+
+}
