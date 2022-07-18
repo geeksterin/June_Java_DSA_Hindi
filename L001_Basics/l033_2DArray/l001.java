5d4fce67b5059c547b61f420981f9a4b8a4f2868
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-18 21:23:15 +0530

    update

--- L001_Basics/l033_2DArray/l001.java
+++ L001_Basics/l033_2DArray/l001.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class l001 {
+
+    public static Scanner scn = new Scanner(System.in);
 
     public static void test() {
         // int[][] arr = new int[4][3];
@@ -7,16 +11,29 @@ public class l001 {
         // }
 
         int[][] arr1 = { { 10, 11, 12 }, { 13, 15 }, { 32, 23, 45, 45, 65, 8 } };
+        for (int r = 0; r < arr1.length; r++) {
+            for (int c = 0; c < arr1[r].length; c++) {
+                System.out.print(arr1[r][c] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         int[][] arr2 = { { 1, 2, 3, 4 }, { 11, 12, 13, 14 }, { 21, 22, 23, 24 } };
-
-        int n = ??, m = ??;
+        int n = arr2.length, m = arr2[0].length; // it is valid only for rectangle matrix
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < m; c++) {
                 System.out.print(arr2[r][c] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void print2D(int[][] arr) {
+
+    }
+
+    public static void input2D(int[][] arr) {
 
     }
 
