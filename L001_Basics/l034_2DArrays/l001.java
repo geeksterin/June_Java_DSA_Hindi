05ec7af5ccb85708c4fa2fa01a98dcc0ce7daf31
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-19 20:16:15 +0530

    update

--- L001_Basics/l034_2DArrays/l001.java
+++ L001_Basics/l034_2DArrays/l001.java
@@ -5,18 +5,44 @@ public class l001 {
     public static Scanner scn = new Scanner(System.in);
 
     public static boolean find(int[][] arr, int data) {
-
+        int n = arr.length, m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == data)
+                    return true;
+            }
+        }
+        return false;
     }
 
     public static int maximum(int[][] arr) {
-
+        int n = arr.length, m = arr[0].length, max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+        return max;
     }
 
     public static int minimum(int[][] arr) {
-
+        int n = arr.length, m = arr[0].length, min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                min = Math.min(min, arr[i][j]);
+            }
+        }
+        return min;
     }
 
     public static int sumOfAllElements(int[][] arr) {
+        int n = arr.length, m = arr[0].length, sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+        }
+        return sum;
 
     }
 
