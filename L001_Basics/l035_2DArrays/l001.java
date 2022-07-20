8a8f3652a12f83bc64793f6c54aa48c5f57b9781
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-20 20:50:56 +0530

    update

--- L001_Basics/l035_2DArrays/l001.java
+++ L001_Basics/l035_2DArrays/l001.java
@@ -31,16 +31,35 @@ public class l001 {
 
     // (i_1,j_1) -> cell_1, (i_2,j_2) -> cell_2
     public static void swap2D(int[][] arr, int i_1, int j_1, int i_2, int j_2) {
-
+        int temp = arr[i_2][j_2];
+        arr[i_2][j_2] = arr[i_1][j_1];
+        arr[i_1][j_1] = temp;
     }
 
     // matrix always be a square matrix
     public static void transpose(int[][] matrix) {
+        int n = matrix.length, m = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < m; j++) {
+                swap2D(matrix, i, j, j, i);
+            }
+        }
+    }
+
+    // T: O(N), where N is no of rows
+    public static void swapColumn(int[][] arr, int c1, int c2) {
+
+    }
+
+    // T: O(??)
+    public static void swapRow(int[][] arr, int r1, int r2) {
 
     }
 
     public static void main(String[] args) {
         int[][] arr = input2d();
-
+        print2D(arr);
+        transpose(arr);
+        print2D(arr);
     }
 }
