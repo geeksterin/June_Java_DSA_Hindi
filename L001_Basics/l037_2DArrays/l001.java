5f38aaf88430a86a68c1095b357ab3d4775e9382
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-22 21:46:53 +0530

    update

--- L001_Basics/l037_2DArrays/l001.java
+++ L001_Basics/l037_2DArrays/l001.java
@@ -23,18 +23,37 @@ public class l001 {
         int n = matrix.length, m = matrix[0].length;
         if (n != m)
             return false;
-
-        
-
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < m; j++) {
+                if (matrix[i][j] != matrix[j][i])
+                    return false;
+            }
+        }
 
         return true;
     }
 
-    public static void main(String[] args){
-        int[][] arr = {{1,0,0},
-                       {0,1,0},
-                       {0,0,2}
-                    };
+    public static void printDiagonal(int[][] arr, int r, int c) {
+        int n = arr.length, m = arr[0].length;
+        while (r < n && c < m) {
+            System.out.print(arr[r][c] + " ");
+            r++;
+            c++;
+        }
+        System.out.println();
+    }
+
+    public static void printUpperDiagonal(int[][] arr) {
+        int n = arr.length, m = arr[0].length;
+        for (int j = 0; j < m; j++)
+            printDiagonal(arr, 0, j);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 2 }
+        };
         System.out.println(identityMatrix(arr));
     }
 }
