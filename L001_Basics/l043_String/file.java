2ec3f8803a0d0a2b6676b86f6cbe81a9b2638cd7
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-29 20:55:41 +0530

    update

--- L001_Basics/l043_String/file.java
+++ L001_Basics/l043_String/file.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class file {
 
     public static int countLowerCase(String str) {
@@ -24,12 +26,50 @@ public class file {
 
     // aBCdghi -> ABCDGHI
     public static String allLowerCaseToUpperCase(String str) {
+        StringBuilder ans = new StringBuilder();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                ans.append((char) (ch - 'a' + 'A'));
+            } else {
+                ans.append(ch);
+            }
+        }
 
+        return ans.toString(); // O(n)
     }
 
     // aBCdghi -> abcdghi
     public static String allUpperCaseToLowerCase(String str) {
+        StringBuilder ans = new StringBuilder();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ans.append((char) (ch - 'A' + 'a'));
+            } else {
+                ans.append(ch);
+            }
+        }
 
+        return ans.toString(); // O(n)
+    }
+
+    // aBCdghi -> AbcDGHI
+    public static String toggleCase(String str) {
+        StringBuilder ans = new StringBuilder();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ans.append((char) (ch - 'A' + 'a'));
+            } else if (ch >= 'a' && ch <= 'z') {
+                ans.append((char) (ch - 'a' + 'A'));
+            }
+        }
+
+        return ans.toString();
     }
 
 }
