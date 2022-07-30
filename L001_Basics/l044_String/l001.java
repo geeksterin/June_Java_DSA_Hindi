37272ed01b8c4b963d438a7ad2bfb64426e0f326
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-07-30 14:06:54 +0530

    update

--- L001_Basics/l044_String/l001.java
+++ L001_Basics/l044_String/l001.java
@@ -1,6 +1,6 @@
 public class l001 {
 
-    public String allUpperCaseToLowerCase(String str) {
+    public String toLowerCase(String str) {
         StringBuilder ans = new StringBuilder();
         int n = str.length();
         for (int i = 0; i < n; i++) {
@@ -15,9 +15,25 @@ public class l001 {
         return ans.toString(); // O(n)
     }
 
-    public String capitalizeTitle(String title) {
-        String[] arr = title.split(' ');
+    public static String[] split(String str, String regex) {
+        return new String[0];
+    }
 
+    // 2129
+    public String capitalizeTitle(String title) {
+        String[] arr = title.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            String str = arr[i];
+            if (str.length() <= 1)
+                ans.append(toLowerCase(str) + " ");
+            else {
+
+            }
+        }
+
+        return ans.toString();
     }
 
 }
