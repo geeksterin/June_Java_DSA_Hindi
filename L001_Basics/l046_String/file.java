0d4405b3c4695b507d873b7409df79c0252b56f7
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-08-02 21:34:17 +0530

    update

--- L001_Basics/l046_String/file.java
+++ L001_Basics/l046_String/file.java
@@ -21,14 +21,48 @@ public class file {
         return sb.toString();
     }
 
+    // (a, 4) -> "aaaa"
+    public static String makeString(char ch, int freq) {
+        StringBuilder sb = new StringBuilder();
+        while (freq-- > 0) {
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
     // T: O(N)
     public static String stringSorting_02(String str) {
-        int[] freqArray = new int[128 + 1];
+        int[] freqArray = new int[128];
+        StringBuilder sb = new StringBuilder();
+
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            freqArray[ch] = freqArray[ch] + 1;
+        }
+
+        for (int i = 0; i < 128; i++) {
+            if (freqArray[i] > 0) {
+                sb.append(makeString((char) i, freqArray[i]));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // "12344321" -> sum(1,2,3,4) == sum(4,3,2,1)
+    // check if the string is balanced. Given a string as numeric
+    // characters (“1234”) , check if the sum of integers on the left of the middle
+    // index is equal to the sum of the integers on the right of the middle index.
+    public static boolean isBalance(String str) {
 
     }
 
     public static void main(String[] args) {
-
+        // String statement = scn.nextLine(); // for input statement -> "my name is
+        // rajneesh"
+        // String word = scn.next(); // for input word -> "abcdf"
     }
 
 }
